0d4db02d3eac996dcbdfb45ece7aa94cb1e3e9d0
milonroy26 <101338962+milonroy26@users.noreply.github.com>
2022-07-22 15:28:59 +0600

    todo list all complet

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  todoValue:string = '';
+  list:Todo[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  
+  }
+
+  addItem(){
+    if(this.todoValue !== ''){
+      const newItem: Todo ={
+        id: Date.now(),
+        value: this.todoValue,
+        isDone:false
+      }
+      this.list.push(newItem)
+      console.log(this.list);
+    }
+    this.todoValue = '';
+  }
+  deleteItem(id:number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
+
+}
